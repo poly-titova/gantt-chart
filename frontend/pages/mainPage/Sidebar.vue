bccc8f9d548fd838d94083420e2665055e5e9175
Kseniya <pupkoksenia@gmail.com>
2024-06-13 19:32:57 +0300

    bar view, sidebar add start day, creation task with start day

--- frontend/pages/mainPage/Sidebar.vue
+++ frontend/pages/mainPage/Sidebar.vue
@@ -3,7 +3,7 @@
     v-model="isSidebarVisible"
     title="Задание"
     class="sidebar"
-    style="padding: 0px 20px;"
+    style="padding: 0px 20px"
     @hide="resetState"
   >
     <el-row>
@@ -12,39 +12,44 @@
           placement="bottom-start"
           width="500"
           trigger="click"
-          v-model="visible">
+          v-model="visible"
+        >
           <div>
-            <p style="margin-bottom: 10px; color: #606266; line-height: 1.4; font-size: 14px;">Выберите задачу к которой будет привязана открытая задача:</p>
+            <p
+              style="
+                margin-bottom: 10px;
+                color: #606266;
+                line-height: 1.4;
+                font-size: 14px;
+              "
+            >
+              Выберите задачу к которой будет привязана открытая задача:
+            </p>
 
             <el-form ref="form" :model="soughtTask">
-              <el-form-item prop="id" style="margin-bottom: 10px;">
+              <el-form-item prop="id" style="margin-bottom: 10px">
                 <el-select
                   v-model="soughtTask.id"
                   filterable
                   placeholder="Найти задачу"
-                  style="width: 474px !important;">
+                  style="width: 474px !important"
+                >
                   <el-option
                     v-for="selectTask in tasks"
                     :key="selectTask.id"
                     :label="selectTask.name"
-                    :value="selectTask.id">
+                    :value="selectTask.id"
+                  >
                   </el-option>
                 </el-select>
               </el-form-item>
 
-              <el-form-item style="margin: 0px;">
-                <el-button
-                  type="success"
-                  size="mini"
-                  @click="makeTask"
-                >
+              <el-form-item style="margin: 0px">
+                <el-button type="success" size="mini" @click="makeTask">
                   <span>Сделать подзадачей</span>
                 </el-button>
 
-                <el-button
-                  size="mini"
-                  @click="visible = false"
-                >
+                <el-button size="mini" @click="visible = false">
                   <span>Отменить</span>
                 </el-button>
               </el-form-item>
@@ -54,7 +59,8 @@
             slot="reference"
             size="small"
             icon="el-icon-share"
-            style="margin: 10px 0px;">
+            style="margin: 10px 0px"
+          >
             Сделать подзадачей
           </el-button>
         </el-popover>
@@ -64,8 +70,9 @@
         <el-button
           size="small"
           icon="el-icon-link"
-          style="margin: 10px 20px;"
-          @click="">
+          style="margin: 10px 20px"
+          @click=""
+        >
           Связать задачу
         </el-button>
       </el-col>
@@ -74,35 +81,33 @@
         <el-button
           size="small"
           icon="el-icon-delete"
-          style="margin: 10px 0px 10px 10px;"
-          @click="deleteTask">
+          style="margin: 10px 0px 10px 10px"
+          @click="deleteTask"
+        >
           Удалить задачу
         </el-button>
       </el-col>
     </el-row>
 
-    <el-form ref="form" :model="item" style="position: relative; height: 100%;">
-      <el-form-item
-        prop="name"
-        label="Название:">
-        <el-input v-model="item.name" size="small"/>
+    <el-form ref="form" :model="item" style="position: relative; height: 100%">
+      <el-form-item prop="name" label="Название:">
+        <el-input v-model="item.name" size="small" />
       </el-form-item>
 
-      <el-form-item
-        prop="description"
-        label="Описание:">
+      <el-form-item prop="description" label="Описание:">
         <el-input
           v-model="item.description"
           type="textarea"
           :rows="2"
-          size="small"/>
+          size="small"
+        />
       </el-form-item>
 
-      <el-form-item
-        label="Исполнитель:"
-        prop="responsible_user_ids"
-      >
-        <orgschema-user-selector v-model="item.responsible_user_ids" :font-size="14" />
+      <el-form-item label="Исполнитель:" prop="responsible_user_ids">
+        <orgschema-user-selector
+          v-model="item.responsible_user_ids"
+          :font-size="14"
+        />
       </el-form-item>
 
       <el-form-item label="Планируемое время:" prop="time_plan">
@@ -113,12 +118,21 @@
         <el-input-number v-model="item.time_fact" size="small" />
       </el-form-item>
 
-      <el-form-item style="position: absolute; bottom: 10px; width: 100%;">
+      <el-form-item label="Начало задачи:" prop="start_time">
+        <el-date-picker
+          v-model="item.start_date"
+          @change="handleDateChange"
+          placeholder="Выберите дату"
+        />
+      </el-form-item>
+
+      <el-form-item style="position: absolute; bottom: 10px; width: 100%">
         <el-row :gutter="10">
           <el-col :span="12">
             <el-button
-              style="position: relative; width: 100%; margin-right: 10px;"
-              @click="close">
+              style="position: relative; width: 100%; margin-right: 10px"
+              @click="close"
+            >
               <span>Отменить</span>
             </el-button>
           </el-col>
@@ -127,7 +141,7 @@
             <el-button
               type="success"
               plain
-              style="position: relative; width: 100%; margin: 0px 10px;"
+              style="position: relative; width: 100%; margin: 0px 10px"
               @click="submit"
             >
               <span>Сохранить</span>
@@ -145,6 +159,7 @@ export default {
     value: Object,
     apiKey: String,
     tasks: Array,
+    currentProject: Object,
   },
   data() {
     return {
@@ -154,16 +169,28 @@ export default {
       task: {},
       item: {
         id: 0,
-        name: '',
-        description: '',
+        name: "",
+        description: "",
         responsible_user_ids: [],
         time_plan: 0,
         time_fact: 0,
         parent_id: 0,
-      }
+        start_date: "",
+      },
+      prevStartDate: null,
     };
   },
   methods: {
+    handleDateChange(value) {
+      const date = new Date(value).getTime();
+      let startDate = new Date(this.currentProject.start_date).getTime();
+      let finishDate = new Date(this.currentProject.finish_date).getTime();
+      let isAvailable = date >= startDate && date <= finishDate;
+      if (!isAvailable) {
+        this.item.start_date = this.prevStartDate ?? "";
+        this.$message.error("Эта дата недоступна для выбора.");
+      } else this.prevStartDate = this.item.start_date;
+    },
     open(task) {
       this.task = task;
       this.item = {
@@ -174,64 +201,97 @@ export default {
         time_plan: task.time_plan,
         time_fact: task.time_fact,
         parent_id: task.parent_id,
+        start_date: task.start_date,
       };
       this.isSidebarVisible = true;
+      this.prevStartDate = task.start_date;
     },
     close() {
       this.isSidebarVisible = false;
     },
     async deleteTask() {
-      await $platform.api.requestRoute('tasks.api.task.remove', {key: this.apiKey }, {
-        "id": this.task.id,
-        "is_edit_further": true,
-      });
-      this.$uiNotify.error('Задание удалено');
-      this.$emit('load', true);
+      await $platform.api.requestRoute(
+        "tasks.api.task.remove",
+        { key: this.apiKey },
+        {
+          id: this.task.id,
+          is_edit_further: true,
+        }
+      );
+      this.$uiNotify.error("Задание удалено");
+      this.$emit("load", true);
       this.close();
     },
     async makeTask() {
-      await $platform.api.requestRoute('tasks.api.task.update', { key: this.apiKey }, {
-        "id": this.item.id,
-        "fields": { parent_id: this.soughtTask.id },
-        "fields_old": { parent_id: this.item.parent_id },
-        "is_edit_further": null,
-        "start_date_from_plan": null,
-      });
-      this.$uiNotify.success('Задание отредактированно');
-      this.$emit('load', true);
+      let fields = new Object();
+      let fields_old = new Object();
+      if (this.soughtTask.id !== this.item.parent_id) {
+        fields.parent_id = this.soughtTask.id;
+        fields_old.parent_id = this.item.parent_id;
+      }
+      await $platform.api.requestRoute(
+        "tasks.api.task.update",
+        { key: this.apiKey },
+        {
+          id: this.item.id,
+          fields: fields,
+          fields_old: fields_old,
+          is_edit_further: null,
+          start_date: this.item.start_date,
+        }
+      );
+      this.$uiNotify.success("Задание отредактированно");
+      this.$emit("load", true);
       this.visible = false;
     },
     async submit() {
-      let fields = new Object();
-      let fields_old = new Object();
+      const projectEnd = new Date(this.currentProject.finish_date);
+      const taskStart = new Date(this.item.start_date);
 
-      if (this.task.name !== this.item.name) {
-        fields.name = this.item.name;
-        fields_old.name = this.task.name;
+      const taskEnd = new Date(taskStart);
+      taskEnd.setDate(taskEnd.getDate() + Number(this.item.time_plan));
+
+      if (projectEnd < taskEnd) {
+        this.$message.error("Ошибка");
+      } else {
+        let fields = new Object();
+        let fields_old = new Object();
+
+        if (this.task.name !== this.item.name) {
+          fields.name = this.item.name;
+          fields_old.name = this.task.name;
+        }
+
+        if (this.task.description !== this.item.description) {
+          fields.description = this.item.description;
+          fields_old.description = this.task.description;
+        }
+
+        if (this.task.responsible_user_ids !== this.item.responsible_user_ids) {
+          fields.responsible_user_ids =
+            this.item.responsible_user_ids.length == 0
+              ? this.item.responsible_user_ids
+              : [this.item.responsible_user_ids];
+          fields_old.responsible_user_ids = [...this.task.responsible_user_ids];
+        }
+
+        if (this.task.time_plan !== this.item.time_plan) {
+          fields.time_plan = this.item.time_plan;
+          fields_old.time_plan = this.task.time_plan;
+        }
+
+        if (this.task.time_fact !== this.item.time_fact) {
+          fields.time_fact = this.item.time_fact;
+          fields_old.time_fact = this.task.time_fact;
+        }
+
+        if (this.task.start_date !== this.item.start_date) {
+          fields.start_date = this.item.start_date;
+          fields_old.start_date = this.task.start_date;
+        }
+        this.$emit("input", this.item, fields, fields_old);
+        this.close();
       }
-
-      if (this.task.description !== this.item.description) {
-        fields.description = this.item.description;
-        fields_old.description = this.task.description;
-      }
-
-      if (this.task.responsible_user_ids !== this.item.responsible_user_ids) {
-        fields.responsible_user_ids = this.item.responsible_user_ids.length == 0 ? this.item.responsible_user_ids : [this.item.responsible_user_ids];
-        fields_old.responsible_user_ids = [...this.task.responsible_user_ids];
-      }
-
-      if (this.task.time_plan !== this.item.time_plan) {
-        fields.time_plan = this.item.time_plan;
-        fields_old.time_plan = this.task.time_plan;
-      }
-
-      if (this.task.time_fact !== this.item.time_fact) {
-        fields.time_fact = this.item.time_fact;
-        fields_old.time_fact = this.task.time_fact;
-      }
-
-      this.$emit('input', this.item, fields, fields_old);
-      this.close();
     },
   },
 };
--- frontend/pages/mainPage/chart/Chart.vue
+++ frontend/pages/mainPage/chart/Chart.vue
@@ -6,44 +6,60 @@
       @current-change="handleCurrentChange"
       style="width: 100%"
       row-key="id"
-      border>
-      <el-table-column
-        label="Номер"
-        prop="index"
-        width="77">
-      </el-table-column>
-      <el-table-column
-        label="Наименование"
-        width="279">
+      border
+    >
+      <el-table-column label="Номер" prop="index" width="77"> </el-table-column>
+      <el-table-column label="Наименование" width="279">
         <template slot-scope="scope">
-          <el-popover
-            trigger="hover"
-            placement="right"
-            width="370">
+          <el-popover trigger="hover" placement="right" width="370">
             <div>
-              <div style="text-align: right; margin: 0; padding: 4px 4px 0px 0px ">
-                <i class="el-icon-edit" style="padding: 1px 6px;" @click="openSidebar(scope.row)"></i>
-                <i class="el-icon-close" style="margin-left: 20px; padding: 8px 0px;" @click="visible = false"></i>
+              <div
+                style="text-align: right; margin: 0; padding: 4px 4px 0px 0px"
+              >
+                <i
+                  class="el-icon-edit"
+                  style="padding: 1px 6px"
+                  @click="openSidebar(scope.row)"
+                ></i>
+                <i
+                  class="el-icon-close"
+                  style="margin-left: 20px; padding: 8px 0px"
+                  @click="visible = false"
+                ></i>
               </div>
-              <div style="padding: 3px 12px 15px 12px;">
-                <h1 style="padding: 7px 5px; font-size: 14px; font-weight: 505; line-height: 1.4;">{{ scope.row.name }}</h1>
-                <p style="padding: 7px 5px; line-height: 1.4;">{{ getText(scope.row.description) }}</p>
+              <div style="padding: 3px 12px 15px 12px">
+                <h1
+                  style="
+                    padding: 7px 5px;
+                    font-size: 14px;
+                    font-weight: 505;
+                    line-height: 1.4;
+                  "
+                >
+                  {{ scope.row.name }}
+                </h1>
+                <p style="padding: 7px 5px; line-height: 1.4">
+                  {{ getText(scope.row.description) }}
+                </p>
               </div>
-              <div style="margin: 3px 6px 0px 6px; color: #9597B2">
+              <div style="margin: 3px 6px 0px 6px; color: #9597b2">
                 Исполнители: {{ getProfile(scope.row.responsible_user_ids) }}
                 {{ scope.row.project }}
               </div>
             </div>
-            <div slot="reference" class="name-wrapper">{{ scope.row.name }}</div>
+            <div slot="reference" class="name-wrapper">
+              {{ scope.row.name }}
+            </div>
           </el-popover>
         </template>
       </el-table-column>
     </el-table>
-  
+
     <sidebar
       ref="sidebar"
       @input="updateTask"
       @load="onload"
+      :currentProject="currentProject"
       :apiKey="apiKey"
       :tasks="tasks"
     />
@@ -51,13 +67,14 @@
 </template>
 
 <script>
-import Sidebar from './../Sidebar.vue';
+import Sidebar from "./../Sidebar.vue";
 
 export default {
   props: {
     apiKey: String,
     tasks: Array,
     profiles: Array,
+    currentProject: Object,
   },
   data() {
     return {
@@ -71,13 +88,17 @@ export default {
   },
   computed: {
     dataTable() {
-      let tasksChildfree = this.tasks.filter(task => task.parent_data == null);
+      let tasksChildfree = this.tasks.filter(
+        (task) => task.parent_data == null
+      );
       tasksChildfree.map((task, index) => {
         task.index = index + 1;
         let arrChildren = [];
         if (task.checklist_items.lenght != Array(0)) {
           task.checklist_items.map((item, index) => {
-            let childrenTask = this.tasks.find(arr => arr.id == item.linked_task_id);
+            let childrenTask = this.tasks.find(
+              (arr) => arr.id == item.linked_task_id
+            );
             childrenTask.index = `${task.index}.${index + 1}`;
             arrChildren.push(childrenTask);
           });
@@ -86,11 +107,11 @@ export default {
       });
 
       return tasksChildfree;
-    }
+    },
   },
   methods: {
     onload() {
-      this.$emit('load', true);
+      this.$emit("load", true);
     },
     handleCurrentChange(val) {
       this.currentRow = val;
@@ -100,15 +121,19 @@ export default {
       this.$refs.sidebar.open(item);
     },
     async updateTask(item, fields, fields_old) {
-      await $platform.api.requestRoute('tasks.api.task.update', {key: this.apiKey }, {
-        "id": item.id,
-        "fields": fields,
-        "fields_old": fields_old,
-        "is_edit_further": null,
-        "start_date_from_plan": null,
-      });
-      this.$uiNotify.success('Задание отредактированно');
-      this.$emit('load', true);
+      await $platform.api.requestRoute(
+        "tasks.api.task.update",
+        { key: this.apiKey },
+        {
+          id: item.id,
+          fields: fields,
+          fields_old: fields_old,
+          is_edit_further: null,
+          start_date: item.start_date,
+        }
+      );
+      this.$uiNotify.success("Задание отредактированно");
+      this.$emit("load", true);
     },
     getProfile(id) {
       if (this.profiles[id] !== undefined) {
@@ -117,13 +142,13 @@ export default {
     },
     getText(str) {
       if (str !== null) {
-        const startIndex = str.indexOf('<p>') + 3;
-        const endIndex = str.indexOf('</p>', startIndex);
+        const startIndex = str.indexOf("<p>") + 3;
+        const endIndex = str.indexOf("</p>", startIndex);
         const text = str.substring(startIndex, endIndex);
 
         return text;
       }
-      
+
       return str;
     },
   },
@@ -136,19 +161,19 @@ export default {
   border-collapse: collapse;
   width: 100%;
   color: #909399;
-  border: 1px solid #EBEEF5;
+  border: 1px solid #ebeef5;
 }
 
 .table-row {
   display: table-row;
   width: 100%;
-  border-bottom: 1px solid #EBEEF5;
+  border-bottom: 1px solid #ebeef5;
 }
 
 .table-cell {
   display: table-cell;
   padding: 12px 10px;
-  border-right: 1px solid #EBEEF5;
+  border-right: 1px solid #ebeef5;
 }
 
 .table-cell_number {
